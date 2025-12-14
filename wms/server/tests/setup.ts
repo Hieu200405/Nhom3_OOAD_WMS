38624dc7e3472952aaafce26bdb416dd4e843f0a
Horpes66 <sonhothienbao@gmail.com>
2025-12-15 01:03:40 +0700

    add tests

---
+++ wms/server/tests/setup.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let mongod: MongoMemoryServer;
+
+beforeAll(async () => {
+  mongod = await MongoMemoryServer.create();
+  const uri = mongod.getUri();
+  process.env.MONGODB_URI = uri;
+  await mongoose.connect(uri);
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  if (mongod) {
+    await mongod.stop();
+  }
+});
+
+afterEach(async () => {
+  const db = mongoose.connection.db;
+  if (!db) {
+    return;
+  }
+  const collections = await db.collections();
+  await Promise.all(collections.map((collection) => collection.deleteMany({})));
+});
